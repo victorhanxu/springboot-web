f77b1dd22f6f5bf049caa5b4b7c50d511574cf2f
Victor Han <victor.han99@yahoo.ca>
2020-05-20 22:46:09 -0400

    hello()

--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -14,4 +14,10 @@ public class GreetingController {
 		return "greeting";
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello";
+	}
+
 }
